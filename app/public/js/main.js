2506196b2050209abf9a78fe81bc2b065545bfd5
costadvl <costadvl@gmail.com>
2016-12-28 22:21:59 +0100

    (a) angular enviroment initial state.

---
+++ app/public/js/main.js
@@ -0,0 +1,5 @@
+var app = angular.module('base', []);
+app.controller('baseController', function($scope){
+	this.baseModel = 'hey dude'
+});
+
--- models/user.js
+++ models/user.js
@@ -18,20 +18,20 @@ var schemaUser = new Schema({
 	updated_at: Date
 });
 
-//schemaUser.pre('save', function (next) {
-	//var user = this;
-	//if(!user.isModified('password')) return next();
+schemaUser.pre('save', function (next) {
+	var user = this;
+	if(!user.isModified('password')) return next();
 
-	//bcrypt.genSalt(SALT_WORK_FACTOR, function (err,salt) {
-		//if (err) return next(err);
-		//bcrypt.hash(user.password, salt, function (err,hash) {
-			//if(err) return next(err);
+	bcrypt.genSalt(SALT_WORK_FACTOR, function (err,salt) {
+		if (err) return next(err);
+		bcrypt.hash(user.password, salt, function (err,hash) {
+			if(err) return next(err);
 
-			//user.password = hash;
-			//next();
-		//});
-	//});
-//});
+			user.password = hash;
+			next();
+		});
+	});
+});
 
 //schemaUser.methods.comparePassword = function (candidatePassword, cb) {
 	//bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
