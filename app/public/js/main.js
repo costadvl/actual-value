4446815e11ddc3af5d17951c230688512063ef85
costadvl <costadvl@gmail.com>
2017-01-20 14:54:11 +0100

    (!) on master exception

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,5 +1,5 @@
 var app = angular.module('base', []);
 app.controller('baseController', function($scope){
-	this.baseModel = 'hey dude, how are you?'
+	this.baseModel = 'hey dude, no so fine'
 });
 
